d62978e575fced0a4097dc9101d08356978fbe71
79675 <79675@LAPTOP-G63VBJHK>
2022-09-08 16:35:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadacha
+{
+    class Program
+    {
+        static void Main()
+        {
+            var curs = 80;
+            var tryparse = false;
+            var value = 0;
+            while (!tryparse)
+            {
+                Console.Write("Доллары:");
+                var dollar = Console.ReadLine();
+                tryparse = int.TryParse(dollar, out value);
+            }
+            var rubli = value * curs;
+            Console.WriteLine("Курс: 1 доллар = {0} рублей", curs);
+            if(value<500)
+            {
+                Console.WriteLine("Учитывая комиссию 8 рублей, у вас "+(rubli - 8));
+            }
+            else
+            {
+                Console.WriteLine("Учитывая комиссию 3%, у вас " + (rubli - ((rubli * 3) / 100)));
+            }
+        }
+    }
+}
